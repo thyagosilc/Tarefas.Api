8ca5abe80166baf6845e2607296959381f445def
thyago_silc <thyago_silc@outlook.com>
2023-03-21 17:29:58 -0300

    Realizado ajuste.

--- src/Tarefas.Api/ViewModels/TarefaViewModel.cs
+++ src/Tarefas.Api/ViewModels/TarefaViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using Tarefas.Business.Models;
 
 namespace Tarefas.Api.ViewModels
@@ -11,7 +12,9 @@ namespace Tarefas.Api.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(250, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
-        public DateTime Data { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public string Data { get; set; }
         public int Status { get; set; }
     }
 }

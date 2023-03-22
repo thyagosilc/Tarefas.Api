529aea91387125d3fe9f6603a9d7f8043ff5ca53
thyago_silc <thyago_silc@outlook.com>
2023-03-22 07:35:34 -0300

    commit

--- src/Tarefas.Api/ViewModels/TarefaViewModel.cs
+++ src/Tarefas.Api/ViewModels/TarefaViewModel.cs
@@ -14,7 +14,7 @@ namespace Tarefas.Api.ViewModels
         public string Descricao { get; set; }
 
         [DataType(DataType.DateTime)]
-        public string Data { get; set; }
+        public DateTime Data { get; set; }
         public int Status { get; set; }
     }
 }
